b582a20d6d6b7c4137ccd9b74284425d02478a81
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-03 15:20:47 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Sgmedical on 2016/10/3.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
