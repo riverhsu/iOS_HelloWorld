839201295b55c11ce997b95e7694469de8530b62
Sgmedical <sgmedical@sgs-macbook-air.local>
2016-10-17 09:51:11 +0800

    1. Practice UIAlertController function 2. modify width of label

---
+++ HelloWorld/SwitchSegue.swift
@@ -0,0 +1,21 @@
+//
+//  SwitchSegue.swift
+//  HelloWorld
+//
+//  Created by Sgmedical on 2016/10/5.
+//  Copyright © 2016年 Sgmedical. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwitchSequeController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var clickBtn: UIButton!
     @IBOutlet weak var showText: UILabel!
     
+    @IBOutlet weak var linkToSegue: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +30,11 @@ class ViewController: UIViewController {
 
     @IBAction func valueChanged(_ sender: AnyObject) {
         showText.text = "Hello iOS World"
+        let alertController = UIAlertController(title: "Welcome to my first App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        self.present(alertController, animated: true, completion:nil)
     }
 
 }
